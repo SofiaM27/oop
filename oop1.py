8860c27fd49f8924141de2cbfd2957b131ce1c78
SofiaM27 <moskalenkos.22560@gmail.com>
2025-01-24 11:30:49 +0200

    git commit -m "Додано функцію парсингу тексту"

---
+++ oop1.py
@@ -0,0 +1,9 @@
+import re
+from collections import Counter
+
+def parse_text(file_path):
+    """Зчитує текстовий файл і повертає список слів без спеціальних символів."""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read().lower()
+    words = re.findall(r'\b\w+\b', text)  # Вилучення лише слів
+    return words
